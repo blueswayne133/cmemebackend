cac7e8c20f2323ce6a4f5c575692567b7c62912a
blueswayne133 <email-used-for-blueswayne133@github.com>
2025-10-06 03:05:50 +0100

    adding laravel cor

---
+++ config/cors.php
@@ -0,0 +1,20 @@
+return [
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'http://localhost:5173',   // local vite dev
+        'https://cmeme.thspace.io' // production react domain
+    ],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
